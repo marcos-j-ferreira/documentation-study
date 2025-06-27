1c5c91575206bd6a762ee330ef63d05988e0f5f1
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-06-27 11:40:59 -0300

    docs: add JavaDoc to functions

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Calculadora 
+ * 
+ * Realiza as quatros operações da matemática
+ * 
+ * @param {number} x - Primeiro número 
+ * @param {number} y - Segundo número
+ * @return {number} Resultado da operação 
+ */
+
+class Calculadora{
+
+    public final double somar(double x, double y){return x + y; }
+
+    public final double multiplicar(double x, double y){return x * y; } 
+
+    public final double subtrair(double x, double y){return x - y; } 
+
+    public final double dividir(double x, double y){return x / y; } 
+
+}
---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/**
+ *  Arquivo main
+ * 
+ *  - inicia a classe calculadora
+ *  - passa parametro para as operações 
+ *  - exibi o resultado das quatro operações de forma organizada.
+ */
+
+
+public class Main{
+
+    public static void main(String[] args){
+
+        Calculadora cal1 = new Calculadora();
+
+        double resultSoma = cal1.somar(10, 10);
+        double resultMult = cal1.multiplicar(8,10);
+        double ressultSub =  cal1.subtrair(10,5);
+        double ressultDiv = cal1.dividir(20,4);
+
+        System.out.println(" === Resultados das operações basicas ===\n\n Soma de 10 + 10 = "+resultSoma+"\n multiplicação 8 X 10 = "+resultMult+"\n subtração 5 - 10 = "+ ressultSub+ "\n Divisão 20 / 4 = "+ ressultDiv);
+
+    }
+}
